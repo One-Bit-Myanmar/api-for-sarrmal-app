5bc2cf712c9e2bf74d0629a4c76b5cc3df1e0e83
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-09 18:27:21 +0630

    [feat: added food and user models]

---
+++ app/db/models/food.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from datetime import datetime
+
+class Food(BaseModel):
+    food_id: str
+    name: str
+    category: str
+    ingredients: list[str]
+    calories: int
+    food_allergies: list[str]
+    url_to_how_to_cook: str
+    image: str
+    best_time_to_eat: list[str]
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    @validator("updated_at", pre=True, always=True)
+    def set_updated_at(cls, v):
+        return v or datetime.utcnow()
+    def save(self, update: bool = False):
+        if update:
+            self.updated_at = datetime.utcnow()
+    
---
+++ app/db/models/user.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+
+class User(BaseModel):
+    user_id: str
+    username: str
+    email: str
+    password: str
+    weight: float
+    height: float
+    bmi: float
+    age: int
+    diseases: list[str]
+    allergies: list[str]
+    gender: int
+    exercise: str
+    goals: list[str]
+    preferred_foods: list[str]
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    @validator("updated_at", pre=True, always=True)
+    def set_updated_at(cls, v):
+        return v or datetime.utcnow()
+    def save(self, update: bool = False):
+        if update:
+            self.updated_at = datetime.utcnow()
