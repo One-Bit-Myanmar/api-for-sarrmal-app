25c675ffcb742ff33331d7a095bc5b799bfaf211
Pyae-PhyoMaung <mr.phyoemaung007@gmail.com>
2024-08-11 23:24:26 +0630

    [feat: add food history model]

---
+++ app/db/models/food_history.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field, validator
+from typing import Optional
+from datetime import datetime
+
+class Food_history(BaseModel):
+    food_history_id: str
+    food_id: str
+    user_id: str
+    calories: int
+    created_at: datetime = Field(default_factory=datetime.utcnow)
+    updated_at: datetime = Field(default_factory=datetime.utcnow)
+    
+    @validator("updated_at", pre=True, always=True)
+    def set_updated_at(cls, v):
+        return v or datetime.utcnow()
+    def save(self, update: bool = False):
+        if update:
+            self.updated_at = datetime.utcnow()
+
+
+class FoodHistoryItem(BaseModel):
+    datetime: datetime
+    foodID: int
+    calories: int
+
+class UserFoodHistory(BaseModel):
+    id: str
+    userid: str
+    food_history: list[FoodHistoryItem]
