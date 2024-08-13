57771c81d4734cff860a68c82927adcef63097b7
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-14 00:07:32 +0630

    [added: the following methods are added - create access token method for jwt token - verify token mehtod for user auth ]

---
+++ app/core/security.py
@@ -0,0 +1,35 @@
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30 # only about 30 mins
+
+class Hash():
+    @staticmethod
+    def bcrypt(password: str):
+        return pwd_cxt.hash(password)
+    
+    @staticmethod
+    def verify(hashed, normal):
+        return pwd_cxt.verify(normal, hashed)
+    
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+# verify token method (finding the current user which generated the token)
+def verify_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email : str = payload.get("sub")
+        if email  is None:
+            raise credentials_exception
+        return email
+    except JWTError:
+        raise credentials_exception
