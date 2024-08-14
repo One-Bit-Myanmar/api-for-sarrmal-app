6ac2f587be8b6528084e1fd1158995c2e9e99be0
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-14 20:12:56 +0630

    [feat: made sure for code quality]

---
+++ app/api/endpoints/foods.py
@@ -0,0 +1,62 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from pymongo.collection import Collection
+from datetime import datetime
+from app.core.config import connect_to_database
+from io import BytesIO
+from PIL import Image
+import numpy as np
+from typing import List
+
+# get the database 
+from app.db.mongodb import get_db # get the database dependency
+from app.db.models.Food import Food, FoodItem # import the user model
+
+db = connect_to_database()
+db = db["food_recommendation_database"] # database name
+food_collection: Collection = db["foods"] # chat table inside food_recommendation_database
+user_collection: Collection = db["users"] # user table inside food_recommendation_database
+
+router = APIRouter()
+
+# get 3 set of meals (breakfast, lunch, dinner) by user perferences 
+@router.get("/get/recommendations/perferences", response_model=List[FoodItem])
+async def get_recommended_foods():
+    """function to get recommended foods based on user perferences"""
+    pass
+
+
+# get 3 set of meals by user health
+@router.get("/get/recommendations/health", response_model=List[FoodItem])
+async def get_healthy_foods():
+    """func to get healthy foods based on user health"""
+    pass
+
+
+# get selected food detail
+@router.get("/get/{food_id}", response_model=dict)
+async def get_food_details(food_id: str):
+    """func to retrieve food details by id"""
+    pass
+
+
+# tick the eaten food
+@router.put("/tick/{food_id}", response_model=dict)
+async def tick_taken_food(food_id: str):
+    """func to make a food item as eaten"""
+    pass
+
+
+# get the food calores
+@router.get("/get/calories", response_model=dict)
+async def get_food_calories_from_image(file: UploadFile = File(...)):
+    """extract food calories from an uploaded image using a predict"""
+    try:
+        # Read the uploaded image file
+        image = Image.open(BytesIO(await file.read()))
+        
+        # use model to predict the food ID from the image
+        
+        # return the food calories
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
