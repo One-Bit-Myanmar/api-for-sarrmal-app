4f9690ebdc85dfef88c984a593dda782965cd63a
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-16 21:25:36 +0630

    [feat: food ai model that has been added the following function - get claories from image function ]

---
+++ app/models/food.py
@@ -0,0 +1,55 @@
+import re
+import google.generativeai as genai
+from PIL import Image
+import json
+
+# import gemini api key
+from app.core.config import GEMINI_KEY
+
+# config the genai to use
+genai.configure(api_key=GEMINI_KEY)
+
+# Gemini Pro Model
+GEMINI_PRO = genai.GenerativeModel('gemini-pro')
+# Gemini Pro 1.5 Model
+GEMINI_PRO_1O5 = genai.GenerativeModel('gemini-1.5-pro')
+# Regex pattern to match the text outside of the curly 
+PATTERN = r'```json\s*({.*?})\s*```'
+
+
+def get_calories_from_img(image: Image.Image):
+    # generate the response 
+    response = GEMINI_PRO_1O5.generate_content([
+        """what is the total calorie count? Give by the following format
+        {
+        "calories": int,
+        "category": str,
+        "name": str,
+        "ingredients": list[str],
+        "url_to_how_to_cook": str,
+        "image": str,
+        "best_time_to_eat": list[str],
+        }, 
+        so that i can use in my fastapi 
+        response route""",
+        image])
+    # content_text = response["content"]["parts"][0]["text"]
+    content_text = response.text
+    # Clean the response by removing the JSON-like structure
+    match = re.search(PATTERN, content_text, re.DOTALL)
+    if match:
+        # Extract the JSON string
+        json_string = match.group(1)
+        # Convert the string to a Python dictionary
+        return json_string
+    else:
+        raise ValueError("No JSON-like content found in the response.")
+    
+    
+def clean_and_convert_to_json(response_str):
+    try:
+        # Convert the string to a proper JSON object
+        json_dict = json.loads(response_str)
+        return json_dict
+    except json.JSONDecodeError as e:
+        raise ValueError(f"Failed to decode JSON: {str(e)}")
