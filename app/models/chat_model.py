a7b5bf3aa4f55adcd4b5134c9d126d0ced268fa0
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-19 11:01:11 +0630

    [feat: add the handling error for unexpected condition like user location]

--- app/models/chat_model.py
+++ app/models/chat_model.py
@@ -1,5 +1,7 @@
 from app.core.config import GEMINI_KEY 
 import google.generativeai as genai
+from google.api_core import exceptions
+from fastapi import HTTPException
 
 # Configuration
 genai.configure(api_key=GEMINI_KEY)
@@ -13,14 +15,17 @@ generation_config = {
 
 # Function to generate a response using Google Generative AI
 def generate_response(prompt):
-    # init the model
-    model = genai.GenerativeModel(
-        "gemini-pro", 
-        generation_config=generation_config
-    )
-    chat_session = genai.ChatSession(model=model)  # Initialize chat session
-    gemini_response = chat_session.send_message(prompt)
-   # Access text using the correct attribute (replace 'content.parts[0].text' if needed)
-    generated_text = gemini_response.candidates[0].content.parts[0].text  
-    # return the generated text
-    return generated_text
+    try:
+        # init the model
+        model = genai.GenerativeModel(
+            "gemini-pro", 
+            generation_config=generation_config
+        )
+        chat_session = genai.ChatSession(model=model)  # Initialize chat session
+        gemini_response = chat_session.send_message(prompt)
+    # Access text using the correct attribute (replace 'content.parts[0].text' if needed)
+        generated_text = gemini_response.candidates[0].content.parts[0].text  
+        # return the generated text
+        return generated_text
+    except exceptions.FailedPrecondition as e:
+        raise HTTPException(status_code=500, detail=f"Failed to generate response: {e}")
