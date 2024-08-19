66f66796e8fb16435985ed232ad6e640f7ac9079
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-19 19:24:34 +0630

    [feat: updated the following - ensure rate limiting - made sure for user auth - made sure for error handling while user ask unrelated topic - added example responses because chat model is having problem with vpn issue - get all chat worked - get chat by chat_id worked - get chat by current user worked - question to chat model worked but vpn issue not fixed - update chat is worked ]

--- app/api/endpoints/chats.py
+++ app/api/endpoints/chats.py
@@ -3,6 +3,7 @@ from pymongo.collection import Collection
 from datetime import datetime, timedelta
 from app.core.config import connect_to_database
 from typing import List
+from bson import ObjectId
 
 # import slowapi modules
 from app.api.middleware.rate_limiter import limiter
@@ -26,90 +27,115 @@ router = APIRouter()
 
 
 # chat with ai
-@router.post("/chat", response_model=dict)
-@limiter.limit("5/minute")
+@router.post("/chat") # init router 
+@limiter.limit("5/minute") # rate limiting middleware
 async def chat_ai(
-    request: Request,
-    req_msg: RequestModel,
-    current_user: User = Depends(get_current_active_user)
+    request: Request, # without this the limiter won't work
+    req_msg: RequestModel, # request message model this only have partial field 
+    current_user: User = Depends(get_current_active_user) # check for user auth if user is active or not
     ):
     # check user is exist or not
-    user = user_collection.find_one({"user_id": current_user["user_id"]})
+    user = user_collection.find_one({"_id": current_user["_id"]})
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    # get response from open ai layer
-    response = generate_response(req_msg.message)
-    # create chat dict to save in database
+    # # get response from open ai layer
+    response = "temp response from AI"
+    # # response = generate_response(req_msg.message)
+    # # create chat dict to save in database
     chat_dict = {
-        "user_id": str,
+        "user_id": str(current_user["_id"]),
+        "date": datetime.utcnow(),
         "message": req_msg.message,
-        "response": response
+        "response": "temp response from ai",
+        "created_at": datetime.utcnow(),
+        "updated_at": datetime.utcnow()
     }
-    # save to database
-    chat_collection.insert_one(chat_dict)
-    # return 
-    return {"response": "success", "data": chat_dict}
+    # # save to database
+    if chat_collection.insert_one(chat_dict):
+        # # return 
+        return {"response": "success", "data": response}
+    else:
+        raise HTTPException(status_code=401, detail="Failed to save database")
     
 
+
+
 # get the specific chat
-@router.get("/chat/{chat_id}", response_model=dict)
-@limiter.limit("5/minute")
-async def get_chat(request: Request, chat_id: str):
-    chat = chat_collection.find_one({"chat_id": chat_id})
+@router.get("/chat/{chat_id}") # init router
+@limiter.limit("5/minute") # rate limiting middleware
+async def get_chat(
+    request: Request, # without this the limiter won't work
+    chat_id: str, # get the chat id
+    current_user: User = Depends(get_current_active_user) # check for user auth if user is
+    ):
+    # get chat by chat id
+    chat = chat_collection.find_one({"_id": ObjectId(chat_id)})
+    # change objectId to string
+    chat["_id"] = str(chat["_id"])
+    # if chat not exist, then raise exception
     if not chat:
         raise HTTPException(status_code=404, detail="Chat not found")
-    return {"response": chat}
+    return {"response": "success", "data": chat}
 
     
     
 # edit and update the user request
-@router.put("/chat/{chat_id}", response_model=dict)
-@limiter.limit("5/minute")
-async def update_chat(request: Request, chat_id: str, request_msg: RequestModel):
-    # find the chat
-    chat = chat_collection.find_one({"chat_id": chat_id})
+@router.put("/chat/{chat_id}") # init router
+@limiter.limit("5/minute") # rate limiting middleware
+async def update_chat(
+    request: Request, # without this the limiter won't work
+    chat_id: str, # get chat id
+    request_msg: RequestModel, # partial attr of chat model
+    current_user: User = Depends(get_current_active_user) # check for user auth if active or not
+    ):
+    # find the chat by chat id
+    chat = chat_collection.find_one({"_id": ObjectId(chat_id)})
+    # if not found raise the error
     if not chat:
         raise HTTPException(status_code=404, detail="Chat not found")
-
-    # request to open ai api again
     # get response from open ai api
-    
+    response = "updated response"
     # update to database
     chat_collection.update_one(
-        {"chat_id": chat_id}, 
+        {"_id": ObjectId(chat_id)}, 
         {
             "$set": 
             {
                 "message": request_msg.message,
-                # "response": # from response from open ai api key
+                "response": response,
                 "date": datetime.utcnow(),
             }
         }
     )
-    return {"message": "Chat updated successfully", "response": "ai response this"}
+    # return
+    return {"response": "success", "data": response}
     
 
 
-# get the chat history by user id
-@router.get('/chat/history/{user_id}', response_model=dict)
-@limiter.limit("5/minute")
-async def get_chat_history(request: Request, user_id: int) -> list[Chat]:
+
+
+# get the chat history by current user
+@router.get('/chat') # init route
+@limiter.limit("5/minute") # rate limiting middleware
+async def get_chat_history(
+    request: Request, # without this the limiter won't work
+    current_user: User = Depends(get_current_active_user) # for user auth check active or not
+    ):
     # get the start fo day and end of day
     start_of_today = datetime.combine(datetime.today(), datetime.min.time())
     start_of_tomorrow = start_of_today + timedelta(days=1)
-    
-    # get the chats for today
-    chats = chat_collection.find({
-            "user_id": user_id,
-            "datetime": {
+    # get the chats for today ( get as list )
+    chats = list(chat_collection.find({
+        "user_id": str(current_user["_id"]),
+        "date": {
                 "$gte": start_of_today,
                 "$lt": start_of_tomorrow
             }
-        })
-    # convert the cursor to a list 
-    chats_list = list(chats)
-    
-    if not chats_list:
+    }))
+    # if chat not found then raise an error
+    if not chats:
         raise HTTPException(status_code=404, detail="No chats Found")
-    
-    return {"response": chats_list}
+    for item in chats: # change the objectId to string so that we can get from json string
+        item["_id"] = str(item["_id"])
+    # return chats data
+    return {"response": "success", "data": chats}
