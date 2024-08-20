bf21812efb3ae0bb7787f0f650ee747a0bb6ee5b
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-20 20:40:58 +0630

    [update: main file has added the temp food router and requirements.txt updated]

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ from app.db.mongodb import get_db
 from app.api.endpoints.users import router as UserRouter
 from app.api.endpoints.chats import router as ChatRouter
 from app.api.endpoints.foods import router as FoodRouter
-from app.api.endpoints.my_food import router as MyFoodRouter
 from app.api.endpoints.food_histories import router as FoodHistoryRouter
+from app.api.endpoints.temp_end import router as TempFoodRouter
 
 # include middleware are herer
 from app.api.middleware.rate_limiter import add_rate_limit
@@ -43,7 +43,7 @@ app.include_router(UserRouter, tags=["User"], prefix="/api/user") # user route
 app.include_router(ChatRouter, tags=["Chat"], prefix="/api/ai") # chat route
 app.include_router(FoodRouter, tags=["Food"], prefix="/api/food") # food router
 app.include_router(FoodHistoryRouter, tags=["Food History"], prefix="/api/food-history") # food history route
-app.include_router(MyFoodRouter, tags=["New Food"], prefix="/api/new-food") # new food route
+app.include_router(TempFoodRouter, tags=["Temporary Foods"], prefix="/api/temp/food") # temp food router
 
 # calling root directory
 @app.get("/", tags=["Root"])
