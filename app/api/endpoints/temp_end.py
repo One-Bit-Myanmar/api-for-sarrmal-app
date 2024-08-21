f0fd5569470943e59d4009cd81ffdabf5305fcd9
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-21 20:44:19 +0630

    [changes: add response list for temporary store the generated info from food image and add user_id to that response list when user store that generated response to their meal plan list]

--- app/api/endpoints/temp_end.py
+++ app/api/endpoints/temp_end.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException, Request, Depends
+from fastapi import APIRouter, HTTPException, Request, Depends
 from pymongo.collection import Collection
 from app.core.config import connect_to_database
 from typing import List
 from bson import ObjectId
 
+from data.food_sample_data import sample_list 
 
 # import slowapi modules
 from app.api.middleware.rate_limiter import limiter
@@ -34,7 +35,8 @@ async def get_recommended(
     current_user: User = Depends(get_current_active_user) # for active user like auth
     ):
     # get_recommend food from ai generate
-    recommend_food_sets = get_recommended_foods()
+    # recommend_food_sets = get_recommended_foods() # this is main 
+    recommend_food_sets = sample_list
     # if foods set is not none
     if not recommend_food_sets:
         raise HTTPException(status_code=404, detail="Unable to fetch foods, check your internet connection")
