f7927a31238da0e3008ab63e0b4a8be20cd523c0
Hein-HtetSan <heinhtetsan33455@gmail.com>
2024-08-25 01:45:56 +0630

    remove env dir and update config file

--- app/core/config.py
+++ app/core/config.py
@@ -3,19 +3,19 @@ import os
 
 
 # Access environment variables
-CONNECTION_STRING = os.getenv("CONNECTION_STRING")
-HOST_ID = os.getenv("HOST_ID", "0.0.0.0")  # Default to 0.0.0.0 if not set
-PORT_ID = int(os.getenv("PORT_ID", 8000))  # Default to 8000 and ensure it's an integer
-RELOAD_STATE = os.getenv("RELOAD_STATE", "False").lower() == "true"  # Convert to boolean
-DEBUG = os.getenv("DEBUG", "False").lower() == "true"  # Convert to boolean
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-GEMINI_KEY = os.getenv("GEMINI_KEY")
-UNSPLASH_ACCESS_KEY_1 = os.getenv("UNSPLASH_ACCESS_KEY_1")
-UNSPLASH_ACCESS_KEY_2 = os.getenv("UNSPLASH_ACCESS_KEY_2")
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
+CONNECTION_STRING = os.environ["CONNECTION_STRING"]
+HOST_ID = os.environ["HOST_ID"]  # Default to 0.0.0.0 if not set
+PORT_ID = int(os.environ["PORT_ID"])  # Default to 8000 and ensure it's an integer
+RELOAD_STATE = os.environ["RELOAD_STATE"].lower() == "true"  # Convert to boolean
+DEBUG = os.environ["DEBUG"].lower() == "true"  # Convert to boolean
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALGORITHM = os.environ["ALGORITHM"]
+GEMINI_KEY = os.environ["GEMINI_KEY"]
+UNSPLASH_ACCESS_KEY_1 = os.environ["UNSPLASH_ACCESS_KEY1"]
+UNSPLASH_ACCESS_KEY_2 = os.environ["UNSPLASH_ACCESS_KEY2"]
+GOOGLE_CLIENT_ID = os.environ["GOOGLE_CLIENT_ID"]
+GOOGLE_CLIENT_SECRET = os.environ["GOOGLE_CLIENT_SECRET"]
+GOOGLE_REDIRECT_URI = os.environ["GOOGLE_REDIRECT_URI"]
 
 # get connection
 def connect_to_database():
